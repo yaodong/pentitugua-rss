19f3314242bd110f43ed12cce6a416346fcad2bc
newsboy <newsboy@pentitutgua.com>
2015-01-05 12:41:28 +0800

    update at 2015-01-05 12:40:57

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 wget http://www.dapenti.com/blog/rssfortugua.asp -O rss.xml
 if [ -n "$(git status rss.xml --porcelain)" ]; then
   date=$(date "+%Y-%m-%d %H:%M:%S")
-  sed "s/<small><\/small>/<small>$date<\/small>/" index.html
+  sed -i.bak "s/<small>[^<]*<\/small>/<small>$date<\/small>/" index.html
+  rm index.html.bak
   git add rss.xml index.html
-  git commit -m "update at $data"
+  git commit -m "update at $date"
   git push
 fi
