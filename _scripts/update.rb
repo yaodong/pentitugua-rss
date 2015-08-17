ae785adbd1e87fd653ae2772b3acc95780e27ca8
newsboy <newsboy@pentitutgua.com>
2015-08-17 23:38:58 +0800

    use https url

--- _scripts/update.rb
+++ _scripts/update.rb
@@ -2,12 +2,28 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'net/http'
+require 'openssl'
 
-base_url  = 'http://www.dapenti.com/blog'
+# read url and ignore SSL verify
+def read_url(url)
+  uri = URI(url)
+  response = nil
+  Net::HTTP.start(
+      uri.host, uri.port,
+      :use_ssl => uri.scheme == 'https',
+      :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+    request  = Net::HTTP::Get.new uri.request_uri
+    response = https.request request
+  end
+  response.body
+end
+
+base_url  = 'https://www.dapenti.com/blog'
 list_url  = "#{base_url}/blog.asp?subjectid=70&name=xilei"
 keyword   = '喷嚏图卦'
 
-list_html = Nokogiri::HTML(open(list_url))
+list_html = Nokogiri::HTML(read_url(list_url))
 links = list_html.css 'div[align="left"] ul li a'
 links.each do |link|
   if link.text.include? keyword
@@ -21,15 +37,18 @@ links.each do |link|
     file = "#{__dir__}/../_posts/#{year}-#{month}-#{day}-#{year}#{month}#{day}.md"
 
     unless File.exist? file
+
+      puts "Downloading #{link.text}."
+
       url       = "#{base_url}/#{link['href']}"
-      article   = Nokogiri::HTML(open(url), nil, 'gbk')
+      article   = Nokogiri::HTML(read_url(url), nil, 'gbk')
       date_time = article.css('div[align="right"] span.oblog_text').first.text[/[\d-]+ [\d:]+/]
 
       content   = "---\n"
       content   += "layout: post\n"
       content   += "title: \"#{link.text}\"\n"
       content   += "date: #{date_time}\n"
-      content   += "link: #{url}\n"
+      content   += "link: #{url.gsub('https', 'http')}\n"
       content   += "---\n\n"
       content   += article.css('div.oblog_text').first.to_s.encode('utf-8')
 
