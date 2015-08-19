c82d0b51297b1d86d4dfa5728f147df3d1cfc9de
newsboy <newsboy@pentitutgua.com>
2015-08-19 08:33:59 +0800

    Fix path for git add and set commit author.

--- _scripts/scheduled.sh
+++ _scripts/scheduled.sh
@@ -7,13 +7,10 @@ cd $DIR
 git fetch origin
 git reset --hard origin/gh-pages
 
-git config user.name Scheduler
-git config user.email scheduler@pentitugua.com
-
 date=$(date "+%Y-%m-%d %H:%M:%S")
 
 ruby ./update.rb
 
-git add _posts
-git commit -m "update at $date"
+git add ../_posts
+git commit -m "update at $date" --author="Scheduler <scheduler@pentitugua.com>"
 git push -u origin gh-pages
