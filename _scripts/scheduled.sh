ab7b21968b9794b905bc3c933d95e67ea5756bd0
newsboy <newsboy@pentitutgua.com>
2016-04-05 00:51:50 -0600

    Fix scheduled shell

--- _scripts/scheduled.sh
+++ _scripts/scheduled.sh
@@ -12,10 +12,8 @@ git config --local user.email 'newsboy@pentitutgua.com'
 git fetch origin
 git reset --hard origin/gh-pages
 
-date=$(date "+%Y-%m-%d %H:%M:%S")
-
 ruby ./update.rb
 
 git add ../_posts
-git commit -m "update at $date"
+git commit -m "update at $(date '+%Y-%m-%d %H:%M:%S')"
 git push -u origin gh-pages
